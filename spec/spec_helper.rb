99ded7c828e229e8987fec38d963e276c1dbc169
Ricard Forniol <ricard.forniol@sage.com>
2025-08-01 11:59:48 +0200

    Improve coverage check scope

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,15 @@
-require 'simplecov'
-require 'simplecov-cobertura'
+if ENV['COVERAGE'] || ENV['CI']
+  require 'simplecov'
+  require 'simplecov-cobertura'
 
-SimpleCov.start do
-  add_filter '/spec/'
-  add_group 'Libraries', 'lib'
-end
+  SimpleCov.start do
+    add_filter '/spec/'
+    add_group 'Libraries', 'lib'
+  end
 
-if ENV['CI']
-  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+  if ENV['CI']
+    SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+  end
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
